72797618b8aeb331573a51c5486c45e21b407cfe
Tim Shu <tims11105@gmail.com>
2023-03-31 15:52:39 +1100

    Created new quick sort and sorted functions

--- sorting.py
+++ sorting.py
@@ -1,44 +1,126 @@
 from random import randint
+from datetime import datetime
+import time
 
-n = 100
+n = 10
 r = 1000
 
 test_data = [randint(0, r) for n in range(n)]
-print(test_data)
-def bubble_sort(array):
-    for n in range(len(array)-2):
-        sort_position = 1
-        for ind in range(len(array)-sort_position):
-            if array[ind] < array[ind+1]:
-                swap(ind, ind+1, array)
-    pass
 
-def insertion_sort(array):
+def sort_insert(array, key = lambda x:x) -> None:
     for n in range(1, len(array)):
         current_position = n
         temp = array[current_position]
-        while temp > array[current_position -1] and current_position >=1:
+        while key(temp) > key(array[current_position -1]) and current_position >=1:
             array[current_position] = array[current_position -1]
             current_position -= 1
         array[current_position] = temp
     pass
 
-def selection_sort(array):
+def sorted_insert(array, key = lambda x:x) -> list:
+    #duplicate the array
+    new_array = array[:]
+    for n in range(1, len(new_array)):
+        current_position = n
+        temp = new_array[current_position]
+        while key(temp) > key(new_array[current_position -1]) and current_position >=1:
+            new_array[current_position] = new_array[current_position -1]
+            current_position -= 1
+        new_array[current_position] = temp
+    return new_array
+
+def sort_select(array, key = lambda x:x) -> None:
     for n in range(len(array)):
         current_ind = n
         minima = current_ind
-        for i in range(n, len(array)-1):
-            if array[minima] >= array[i]:
+        for i in range(n, len(array)):
+            if key(array[minima]) >= key(array[i]):
                 minima = i
         swap(minima, current_ind, array)
 
+def sorted_select(array, key = lambda x:x) -> list:
+    #duplicate the array
+    new_array = array[:]
+    for n in range(0,len(new_array)):
+        current_ind = n
+        minima = current_ind
+        for i in range(n, len(new_array)):
+            if key(new_array[minima]) >= key(new_array[i]):
+                minima = i
+        swap(minima, current_ind, new_array)
+    return new_array
 
-def swap(position1, position2, array):
+def sorted_quick(array, key):
+    new_array = array[:]
+    return sort_quick(new_array, key)
+
+#This is a recursive sorting method
+def sort_quick(array, key = lambda x:x) -> list:
+    #This is the end of the recursion
+    if len(array) <= 1:
+        #return array
+        return
+
+    #choose an element of the unsorted
+    pivot_element = array[-1]
+    #print(pivot_element)
+    large_pointer = None
+    for index in range(0, len(array)-1):
+        value = array[index]
+        if large_pointer == None:
+            if key(value) > key(pivot_element):
+                large_pointer = index
+                continue
+            continue
+        if key(value) < key(pivot_element):
+            swap(large_pointer, index, array)
+            large_pointer += 1
+            
+    # change the middle index with the pivot element
+    if large_pointer == None:
+        # then the array must all be smalled than the pivot element
+        # We'll take away the pivot and then pass the rest of the array through the function
+        left_side = array[:-1]
+        sort_quick(left_side)
+        array[:-1] = left_side
+        array[-1] = pivot_element
+        return
+
+    swap(-1, large_pointer, array)
+
+    if large_pointer == 0:
+        # then the array must all lbe bigger than the pivot element
+        right_side = array[1:]
+        sort_quick(right_side)
+        array[0] = pivot_element
+        array[1:] = right_side
+        return
+
+    #split the array into left and right
+
+    #left side
+    left_side = array[:large_pointer]
+    sort_quick(left_side)
+
+    #right side
+    right_side = array[large_pointer+1:]
+    sort_quick(right_side)
+
+    # adding the sorted left side, the middle and the sorted right side
+    array[:large_pointer] = left_side
+    array[large_pointer] = pivot_element
+    array[large_pointer+1:] = right_side
+    return
+
+
+
+def swap(position1, position2, array) -> None:
     temp =  array[position1]
     array[position1] = array[position2]
     array[position2] = temp
 
 if __name__ == "__main__":
-    selection_sort(test_data)
-    print(f'This is sorted: \n {test_data}')
+    #print(test_data)
+    pass
+    
     
